d9c0db2007f1263f7d49a645810e1247e7241f47
sraut1104 <rautsudip7928@gmail.com>
2024-05-02 09:08:52 +0530

    Leaning about more about arrow Func and IIFE

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -29,46 +29,73 @@ const user = {
 */
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-console.log(this); // OUTPUT:  {}
+// console.log(this); // OUTPUT:  {}
 /* NOTE: In node-environment,this will refers to 'empty object or {}' buut in the browser's console this referss to the window, as in browser Golobal object is window */
 
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~   Concept of arrow function   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-// function chai(){
-//     let username = "hitesh"
-//     console.log(this.username);
-// }
-
-// chai()
-
-// const chai = function () {
-//     let username = "hitesh"
-//     console.log(this.username);
-// }
-
-const chai =  () => {
-    let username = "hitesh"
-    console.log(this);
+function greenChai(){
+    let username = "sudip"
+    console.log(this);  // See the OUTPUT by running it
+    console.log(this.username); //OUTPUT: undefined
 }
+// greenChai()
 
 
-// chai()
-
-// const addTwo = (num1, num2) => {
-//     return num1 + num2
-// }
-
-// const addTwo = (num1, num2) =>  num1 + num2
-
-// const addTwo = (num1, num2) => ( num1 + num2 )
-
-const addTwo = (num1, num2) => ({username: "hitesh"})
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+const masalaChai = function () {
+    let username = "sudip"
+    console.log(this);  // See the OUTPUT by running it
+    console.log(this.username); //OUTPUT: undefined
+}
+// masalaChai();
 
 
-// console.log(addTwo(3, 4))
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  Function in ES6 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+const adrakChai =  () => { // Arrow Function
+    let username = "sudip"
+    console.log(this);  // OUTPUT: {} or empty object
+    console.log(this.username); //OUTPUT: undefined
+}
+// adrakChai()
 
 
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  Pure Charcha in Arrow Function  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Syntax of Arrow Funtion: () => {}
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~   Basics Structure of Arrow Function  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~   Exlicit Return Arrow Function  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+const addTwoEx1 = (num1, num2) => {
+    return num1 + num2
+}
+// console.log(addTwoEx1(3, 4)) //OUTPUT: 7
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~   Implicit Return Arrow Function  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+const addTwoEx2 = (num1, num2) =>  num1 + num2
+// console.log(addTwoEx2(3, 4)) //OUTPUT: 7
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+/* NOTE: In Arrow Function, after fat arrow  
+- If code is wrapped by curly braces, then we have to write return keyword
+- But code is wrapped by paranthesis, then we don't need to write return keyword.*/
+
+const addTwoEx3 = (num1, num2) => ( num1 + num2 )
+// console.log(addTwoEx3(3, 4)) //OUTPUT: 7
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~   Benifit of Paranthesis  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+const wrongReturnObject = () => {username: "hitesh"}
+// console.log(wrongReturnObject()) // OUTPUT: undefined
+
+const returnObject = () => ({username: "hitesh"})
+// console.log(returnObject()) // OUTPUT: { username: 'hitesh' }
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~   Use of Arrow Functon  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // const myArray = [2, 5, 3, 7, 8]
 
-// myArray.forEach()
+// myArray.forEach( function {})
+// myArray.forEach( () => {})
+// myArray.forEach( () => ())
--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,12 +1,64 @@
-// Immediately Invoked Function Expressions (IIFE)
+// +++++++++++++++++++++++++++++++++++  Immediately Invoked Function Expressions (IIFE)  +++++++++++++++++++++++++++++++++++++++++++
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~   Normal Function Ddefination and Call ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+function normalChai(){
+    console.log(`normal DB CONNECTED one`);
+}
+
+normalChai();
 
 
-(function chai(){
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+/* NOTE: Synatx of IIFE
+()() == Here Function Defination will occur in beteen 1st Paranthesis and 2nd Paranthesis is Functon Execution  
+*/
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+(function masalaChai(){
     // named IIFE
-    console.log(`DB CONNECTED`);
-})();
+    console.log(`masala DB CONNECTED Two`);
+})();  // Semicolon(;) is necessary. To understand Go through below concept
+
+
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~    How to Write Two IIFE One by One  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//To see error Comment all the code expect below two immediately invoke functtion code
+
+// (function masalaaChai(){
+//     // named IIFE
+//     console.log(`masalaa DB CONNECTED Three`);
+// })()
+
+// (function masalaAurChai(){ // TypeError: (intermediate value)(...) is not a function
+//     // named IIFE
+//     console.log(`masalaAur DB CONNECTED Four`);
+// })()
+
+/* NOTE: 
+-- To write 'two IIFE one by one', we have to end 1st IIFE by applying semicolon(;), after that we have to write 2nd IIFE
+-- Though Immediately Invoked Function invokes immidiately, but it doesn't know in which context it has to stop.That's why in this case we have to end this by explicitly using  semi-colon(;) i.e. jS will not add it automatically */
+
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+/* NOTE: 
+-- IIFE is that whic invokes immediately
+-- and To avoid 'Global Scope Pollution' we use IIFE
+*/
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+( () => {
+    // Unnammed-IIFE
+    console.log(`DB CONNECTED Five`);
+} )();
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  Hot to Pass Parameter to IIFE   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 ( (name) => {
-    console.log(`DB CONNECTED TWO ${name}`);
-} )('hitesh')
+    // Unnammed-IIFE
+    console.log(`DB CONNECTED Six ${name}`);
+} )('sudip')
 
